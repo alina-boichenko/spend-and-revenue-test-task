4fd3780b10baca3ee9a5bdb004108ede96610235
Alina Boichenko <ilub1108@gmail.com>
2023-09-28 15:08:05 +0300

    Implemented a view for RevenueStatistic model

--- revenue/views.py
+++ revenue/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.db.models import Sum
+from drf_spectacular.utils import extend_schema
+from rest_framework.generics import ListAPIView
 
-# Create your views here.
+from revenue.models import RevenueStatistic
+from revenue.serializers import RevenueStatisticSerializer
+
+
+class RevenueStatisticViewSet(ListAPIView):
+    queryset = RevenueStatistic.objects.all()
+    serializer_class = RevenueStatisticSerializer
+
+    def get_queryset(self):
+        queryset = (
+            self.queryset.select_related("spend")
+            .values("date", "name")
+            .annotate(
+                sum_revenue=Sum("revenue"),
+                sum_spend=Sum("spend__spend"),
+                sum_impressions=Sum("spend__impressions"),
+                sum_clicks=Sum("spend__clicks"),
+                sum_conversion=Sum("spend__conversion"),
+            )
+        )
+
+        return queryset
+
+    @extend_schema(
+        description="Get a list of revenue statistics with aggregated sums of values."
+    )
+    def get(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
