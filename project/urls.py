0ff20b3f9071b0bea684d624d91bd188209e1ef6
Alina Boichenko <ilub1108@gmail.com>
2023-09-28 15:11:00 +0300

    Added swagger

--- project/urls.py
+++ project/urls.py
@@ -15,9 +15,26 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("revenue", include("revenue.urls", namespace="revenue")),
-    path("spend", include("spend.urls", namespace="spend")),
+    path("api/revenue/", include("revenue.urls", namespace="revenue")),
+    path("api/spend/", include("spend.urls", namespace="spend")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/schema/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger",
+    ),
+    path(
+        "api/schema/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
